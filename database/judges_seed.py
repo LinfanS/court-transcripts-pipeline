64c87ccdda7e3422570b426e14f759db71f25c73
MilesDrabwell <163558169+MilesDrabwell@users.noreply.github.com>
2024-08-14 10:40:21 +0100

    seeded judges

--- database/judges_seed.py
+++ database/judges_seed.py
@@ -6,6 +6,9 @@ from psycopg2.extras import RealDictCursor, execute_values
 from os import getenv
 from dotenv import load_dotenv
 
+COMMON_JUDGE_WORDS = ("Senior", "(Magistrates’", " His", "Master", 'King’s', "(The", "The", "King's", "Remembrancer)", "Bench", "Chief", "Chancery",
+                      "Costs", "Judge", "(Chief", "Taxing", "Master)", "Insolvency", "and", "Companies", "Court", "Registrar", "District", "(MC)", "His", "Her", "Honour", "Tribunal", "Employment", "Deputy", "Magistrate)", "CBE", "DL", "(Deputy", "Lead", "DCRJ)", "Upper", "Recorder", "of", "London,", "Regional", "ICC", "(Magistrates", "(Magistrates'", "Court)", "KC", "Assistant", "Advocate", "General", "Lieutenant", "Colonel", "(Retired)", "JP", "", "OBE")
+
 
 def get_connection() -> connection:
     """
@@ -31,35 +34,35 @@ def get_judge_rows(url: str) -> element:
     return rows
 
 
-def get_bench_judges(url: str) -> list[tuple]:
+def get_bench_judges(url: str) -> list[str]:
     """
     Retrieves all bench judges
     """
-    return [(item.get_text(),) for index, item in enumerate(get_judge_rows(url)) if index % 2 == 1 and item.get_text() != "Name" and item.get_text()[0].isalpha()]
+    return [item.get_text() for index, item in enumerate(get_judge_rows(url)) if index % 2 == 1 and item.get_text() != "Name" and item.get_text()[0].isalpha()]
 
 
-def get_district_judges_magistrates(url: str) -> list[tuple]:
+def get_district_judges_magistrates(url: str) -> list[str]:
     """
     Retrieves all district judge magistrates
     """
-    return [(item.get_text(),) for index, item in enumerate(get_judge_rows(url)) if index % 3 == 0 and item.get_text() != "Judge"]
+    return [item.get_text() for index, item in enumerate(get_judge_rows(url)) if index % 3 == 0 and item.get_text() != "Judge"]
 
 
-def get_diversity_high_court_judges(url: str) -> list[tuple]:
+def get_diversity_high_court_judges(url: str) -> list[str]:
     """
     Retrieves diversity and community judges or high court judges
     """
-    return [(item.get_text(),) for index, item in enumerate(get_judge_rows(url)) if index % 2 == 0 and item.get_text() != "Name" and item.get_text()[0].isalpha()]
+    return [item.get_text() for index, item in enumerate(get_judge_rows(url)) if index % 2 == 0 and item.get_text() != "Name" and item.get_text()[0].isalpha()]
 
 
-def get_judge_advocates(url: str) -> list[tuple]:
+def get_judge_advocates(url: str) -> list[str]:
     """
     Retrieves all judge advocates
     """
-    return [(item.get_text(),) for index, item in enumerate(get_judge_rows(url)) if index % 2 == 0 and item.get_text() != "Judge"]
+    return [item.get_text() for index, item in enumerate(get_judge_rows(url)) if index % 2 == 0 and item.get_text() != "Judge"]
 
 
-def get_circuit_district_judges(url: str) -> list[tuple]:
+def get_circuit_district_judges(url: str) -> list[str]:
     """
     Retrieves all circuit district judges
     """
@@ -70,11 +73,11 @@ def get_circuit_district_judges(url: str) -> list[tuple]:
             name = item.get_text()
             if not item.get_text()[-1].isalpha():
                 name = name[:-1]
-            names.append((name,))
+            names.append(name)
     return names
 
 
-def gather_all_judges() -> list[tuple]:
+def gather_all_judges() -> list[str]:
     """
     Retrieves and combines all judges names
     """
@@ -105,6 +108,18 @@ def gather_all_judges() -> list[tuple]:
     return high_court_masters + bench_chairs + district_judges_magistrates + diversity_community_judges + judge_advocates_general + circuit_judges + district_judges + diversity_community_magistrates
 
 
+def standardise_judge_names(judges: list[str]) -> list[tuple]:
+    standardised_names = []
+    for judge in judges:
+        name_parts = judge.split(" ")
+        good_parts = []
+        for part in name_parts:
+            if part not in COMMON_JUDGE_WORDS and part[0].isalpha():
+                good_parts.append(part)
+        standardised_names.append((" ".join(good_parts),))
+    return standardised_names
+
+
 def upload_judges(conn: connection, judges: list[tuple]) -> None:
     """
     Uploads all the judges names to the database
@@ -116,7 +131,7 @@ def upload_judges(conn: connection, judges: list[tuple]) -> None:
             ;
     """
     with conn.cursor(cursor_factory=RealDictCursor) as curs:
-        execute_values(curs, query, judges)
+        execute_values(curs, query, standardise_judge_names(judges))
         conn.commit()
 
 
@@ -124,4 +139,5 @@ if __name__ == "__main__":
     load_dotenv()
     db_conn = get_connection()
     judges = gather_all_judges()
+    standardise_judge_names(judges)
     upload_judges(db_conn, judges)
