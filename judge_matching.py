1a759aeca815332bf71667eec3c404869e3925c5
MilesDrabwell <163558169+MilesDrabwell@users.noreply.github.com>
2024-08-14 14:01:45 +0100

    added a matching function

---
+++ judge_matching.py
@@ -0,0 +1,50 @@
+from rapidfuzz import process, fuzz
+import psycopg2
+from psycopg2.extensions import connection
+from psycopg2.extras import RealDictCursor
+from os import getenv
+from dotenv import load_dotenv
+
+MATCHING_PERCENT = 95
+
+
+def get_connection() -> connection:
+    """
+    Establishes a connection to the database
+    """
+    return psycopg2.connect(
+        host=getenv("DB_HOST"),
+        user=getenv("DB_USER"),
+        password=getenv("DB_PASSWORD"),
+        database=getenv("DB_NAME"),
+        port=getenv("DB_PORT")
+    )
+
+
+def get_judges(conn: connection) -> list[str]:
+
+    query = """
+            SELECT judge_name FROM judge;
+    """
+
+    with conn.cursor(cursor_factory=RealDictCursor) as curs:
+        curs.execute(query)
+        result = curs.fetchall()
+
+    return [row["judge_name"] for row in result]
+
+
+def match_judge(judge: str, current_judges: list) -> tuple | None:
+    match = process.extractOne(
+        judge, current_judges, score_cutoff=MATCHING_PERCENT, scorer=fuzz.token_set_ratio)
+    if match:
+        return match[0]
+    return None
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    conn = get_connection()
+    judges = get_judges(conn)
+    match = match_judge("Judge Bond", judges)
+    print(match)
