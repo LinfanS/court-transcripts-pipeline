01656c76bae79fa8651a9a50644fb126359b3b05
LinfanS <trainee.linfan.siddiqi@sigmalabs.co.uk>
2024-08-19 01:06:28 +0100

    ensuring only writing invalid gpt responses to file for batch pipeline

--- pipeline/scrap9.py
+++
@@ -1,9 +0,0 @@
-import json
-from ast import literal_eval
-
-
-my_dict = {None: {None: None}}
-
-print(json.loads(json.dumps(my_dict)))  # {'null': {'null': None}}
-
-print(literal_eval(str(my_dict)))  # {None: {None: None}}
