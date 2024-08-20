a2deff15341d5fe0d3a11f439122896e66f7150e
MilesDrabwell <163558169+MilesDrabwell@users.noreply.github.com>
2024-08-20 15:51:49 +0100

    added a file to be put in the pipeline to send emails

---
+++ pipeline/send_emails.py
@@ -0,0 +1,53 @@
+from boto3 import client
+from os import getenv
+from dotenv import load_dotenv
+
+
+def get_sns_client():
+    return client("sns",
+
+                  region_name="eu-west-2",
+
+                  aws_access_key_id=getenv("ACCESS_KEY"),
+
+                  aws_secret_access_key=getenv("SECRET_ACCESS_KEY"))
+
+
+def rename_courts(courts: list[str]) -> list[str]:
+    """
+    Renames the courts to match the topic names on AWS SNS
+    """
+    topic_names = []
+    for court in courts:
+        name = ""
+        name_parts = court.split(" ")
+        new_name = "-".join(name_parts)
+        for letter in new_name:
+            if letter.isalpha() or letter == "-":
+                name += letter
+        topic_names.append(f"c12-courts-{name}")
+    return topic_names
+
+
+def send_emails(table_data: dict[list], client) -> None:
+    """
+    Sends emails to subscribers of the courts being uploaded
+    """
+    uploading_courts = rename_courts(table_data["courts"])
+    for topic_arn in client.list_topics()["Topics"]:
+        arn_parts = topic_arn['TopicArn'].split(":")
+        topic = arn_parts[-1]
+        if topic in uploading_courts:
+            court = topic.replace("-", " ")
+            client.publish(TopicArn=topic_arn["TopicArn"],
+                           Message=f"A new case has been uploaded from the {
+                court[11:]}",
+                Subject=f"New case(s) uploaded")
+
+
+if __name__ == "__main__":
+    sns = get_sns_client()
+    topic_names = rename_courts(
+        ["High Court (Commercial Court)", 'High Court (Circuit Commercial Court)'])
+    send_emails({"courts": ["High Court (Commercial Court)",
+                'High Court (Circuit Commercial Court)']}, sns)
