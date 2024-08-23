5b8a82bc0a8c4ce54661cfff5c089f18a44d5ef1
LinfanS <trainee.linfan.siddiqi@sigmalabs.co.uk>
2024-08-23 02:07:51 +0100

    reorganise db connection from global scope

--- api/api.py
+++ api/api.py
@@ -35,20 +35,19 @@ from pydantic import BaseModel, ConfigDict
 from dotenv import load_dotenv
 import uvicorn
 
-load_dotenv()
-DB_USER = getenv("DB_USER")
-DB_PASSWORD = getenv("DB_PASSWORD")
-DB_HOST = getenv("DB_HOST")
-DB_PORT = getenv("DB_PORT")
-DB_NAME = getenv("DB_NAME")
-
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-engine = create_engine(DATABASE_URL, echo=False)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 
 def get_db():
     """Initialises a new session for each request and close it after"""
+    load_dotenv()
+    DB_USER = getenv("DB_USER")
+    DB_PASSWORD = getenv("DB_PASSWORD")
+    DB_HOST = getenv("DB_HOST")
+    DB_PORT = getenv("DB_PORT")
+    DB_NAME = getenv("DB_NAME")
+
+    DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    engine = create_engine(DATABASE_URL, echo=False)
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
     db = SessionLocal()
     try:
         yield db
